372c4a01b45bf92f5614f8b46d2eadcb8887a96a
keitaro2002 <keitaro1967@icloud.com>
2024-06-11 10:25:32 +0900

    modify dataset.py

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-from config import config
+from src.config import config
 
 class RankineData():
     """
@@ -14,24 +14,37 @@ class RankineData():
     def __init__(self, number_of_prior):
         self.xx, self.yy = np.meshgrid(np.linspace(0, 90, 91), np.linspace(0, 90, 91))
         self.params_true = {'center_x': 45, 'center_y': 45, 'U': 30, 'R': 12}
-        self.emsemble_size = config.emsemble_size
+        self.ensemble_size = config.ensemble_size
         self.number_of_prior = number_of_prior
         self.number_of_obs = config.number_of_obs
         self.radar = np.array([24,24])
-        self.PATH = '/data10/kinuki/da_rankine/data'
+        self.PATH = '/data10/kinuki/da_rankine'
+        
+        self.windvalues_true = None
+        self.windvalues_prior = None
+        self.obs_points = None
+        self.obs = None
 
         # 初期化
         self.initialize_data()
 
     def initialize_data(self):
         files = {
+            'true': 'windvalues_true.npz',
             'prior': f'windvalues_prior{self.number_of_prior}.npz',
             'obs_points': 'obs_points.npy',
             'obs': 'windvalues_obs.npz'
         }
+        # windvalues_trueのデータ
+        true_path = f'{self.PATH}/data/{files["true"]}'
+        if os.path.exists(true_path):
+            self.windvalues_true = self.load_data(files['true'])
+        else:
+            self.windvalues_true = self.wind_speed(self.xx, self.yy, self.params_true)
+            self.save_data(files['true'], self.windvalues_true)
 
         # windvalues_priorのデータ
-        prior_path = f'{self.PATH}/{files["prior"]}'
+        prior_path = f'{self.PATH}/data/{files["prior"]}'
         if os.path.exists(prior_path):
             self.windvalues_prior = self.load_data(files['prior'])
         else:
@@ -41,14 +54,14 @@ class RankineData():
             self.windvalues_prior = self.calculate_windspeeds_prior()
 
         # obs_pointsのデータ
-        obs_points_path = f'{self.PATH}/{files["obs_points"]}'
+        obs_points_path = f'{self.PATH}/data/{files["obs_points"]}'
         if os.path.exists(obs_points_path):
             self.obs_points = self.load_data(files['obs_points'])
         else:
             self.obs_points = self.decide_obs_points()
 
         # obsのデータ
-        obs_path = f'{self.PATH}/{files["obs"]}'
+        obs_path = f'{self.PATH}/data/{files["obs"]}'
         if os.path.exists(obs_path):
             self.obs = self.load_data(files['obs'])
         else:
@@ -58,14 +71,14 @@ class RankineData():
     def save_data(self, filename, data):
         if isinstance(data, dict):
             # 辞書型の場合、np.savezを使用
-            np.savez(f'{self.PATH}/{filename}.npz', **data)
+            np.savez(f'{self.PATH}/data/{filename}.npz', **data)
         else:
             # NumPy型の場合、np.saveを使用
-            np.save(f'{self.PATH}/{filename}.npy', data)
+            np.save(f'{self.PATH}/data/{filename}.npy', data)
 
         
     def load_data(self, filename):
-        return np.load(f'{self.PATH}/{filename}', allow_pickle=True)
+        return np.load(f'{self.PATH}/data/{filename}', allow_pickle=True)
     
     def wind_speed(self, xx, yy, params):
         c_x, c_y, U, R = params['center_x'], params['center_y'], params['U'], params['R']
@@ -85,7 +98,7 @@ class RankineData():
         type: dict型
         key: center_x, center_y, U, R
 
-        それぞれのkeyに対して，emsemble_sizeの数だけの乱数を生成する．
+        それぞれのkeyに対して，ensemble_sizeの数だけの乱数を生成する．
         1. number_of_prior=1,2,3の時
             U, R は値を固定する．
             center_x, center_y は，それぞれの値を平均として，標準偏差がstd_devの正規分布に従う乱数を生成する．
@@ -108,29 +121,30 @@ class RankineData():
             params_prior = {}
             for key in self.params_true.keys():
                 if key == 'U' or key == 'R':
-                    params_prior[key] = np.repeat(self.params_true[key], self.emsemble_size)
+                    params_prior[key] = np.repeat(self.params_true[key], self.ensemble_size)
                 else:
-                    params_prior[key] = np.random.normal(self.params_true[key], std_dev, self.emsemble_size)
+                    params_prior[key] = np.random.normal(self.params_true[key], std_dev, self.ensemble_size)
         
         elif self.number_of_prior == 4:
             params_prior = {}
             for key in self.params_true.keys():
                 if key == 'U' or key == 'R':
-                    params_prior[key] = np.random.normal(self.params_true[key], 3, self.emsemble_size)
+                    params_prior[key] = np.random.normal(self.params_true[key], 3, self.ensemble_size)
                 else:
-                    for i in range(self.emsemble_size):
+                    for i in range(self.ensemble_size):
                         params_prior[key][i] = np.random.normal(self.params_true[key], self.params_true['R'][i])
 
         self.save_data(f'params_prior{self.number_of_prior}', params_prior)
         return params_prior
     
     def calculate_windspeeds_prior(self):
-        windspeeds_prior = {'u_x': np.zeros((self.emsemble_size, 91, 91)),
-                            'u_y': np.zeros((self.emsemble_size, 91, 91)),
-                            'u': np.zeros((self.emsemble_size, 91, 91))}
+        windspeeds_prior = {'u_x': np.zeros((self.ensemble_size, 91, 91)),
+                            'u_y': np.zeros((self.ensemble_size, 91, 91)),
+                            'u': np.zeros((self.ensemble_size, 91, 91))}
         
-        for i in range(self.emsemble_size):
-            wind = self.wind_speed(self.xx, self.yy, self.params_prior[i])
+        for i in range(self.ensemble_size):
+            params = {key: self.params_prior[key][i] for key in self.params_prior}
+            wind = self.wind_speed(self.xx, self.yy, params)
             windspeeds_prior['u_x'][i] = wind['u_x']
             windspeeds_prior['u_y'][i] = wind['u_y']
             windspeeds_prior['u'][i] = wind['u']
@@ -166,7 +180,7 @@ class RankineData():
         vec_obs_radar = np.vstack((vec_obs_radar_x, vec_obs_radar_y)).T
 
         cos = np.sum(housen * vec_obs_radar, axis=1)
-        tru_wind = self.wind_speed(self.xx, self.yy, self.params_true)['u'] * cos
+        tru_wind = self.wind_speed(obs_points[:, 0], obs_points[:, 1], self.params_true)['u'] * cos
         values = np.where(condition, -tru_wind, tru_wind)
         noise = np.random.normal(0, 3, n_obs)
         values_withnoise = values + noise
@@ -185,3 +199,10 @@ class RankineData():
         self.save_data(f'windvalues_obs', obs)
         return obs
 
+if __name__ == '__main__':
+    number_of_prior = 1
+    data = RankineData(number_of_prior)
+    print(data.windvalues_prior['u_x'].shape)
+    print(data.obs_points.shape)
+    print(data.obs['u_x'].shape)
+    print(data.obs['u'].shape)
